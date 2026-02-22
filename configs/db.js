a6d8ad609417e4b009243038ac344c9017527eae
akashshukla748972 <shuklaakash748972@gmail.com>
2026-02-22 23:00:36 +0530

    Modify database connection output message

--- configs/db.js
+++ configs/db.js
@@ -3,8 +3,8 @@ import { gv } from "./global_variable.js";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(gv.MONGO_URI);
-    console.log("database connected successfully!");
+    const conn = await mongoose.connect(gv.MONGO_URI);
+    console.log(`database connected to ${conn.connection.host}`);
   } catch (error) {
     console.error("database connection error:", error);
     process.exit(1);
